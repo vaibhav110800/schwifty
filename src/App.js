bc9812a95e134cd78e7ec40a8c3f7c87fc351d13
vaibhav110800 <vibhu1108@gmail.com>
2023-07-07 01:46:21 +0530

    fix: footer and email added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Banner from './components/Banner';
 import Services from './components/Services';
 import Vision from './components/Vision';
+import Footer  from './components/Footer';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <About />
       <Vision />
       <Services />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,88 @@
-import React from 'react'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { MdLocationPin, MdOutlineEmail, MdOutlinePhone } from "react-icons/md";
+import './footer.css';
 
 const Footer = () => {
-  return (
-    <div>
-      <div></div>
-    </div>
-  )
-}
+  const form = useRef();
 
-export default Footer
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_rc7ja1s",
+        "template_69xzg95",
+        form.current,
+        "cPowQy81g5RPoRa3f"
+      )
+      .then(
+        (result) => {
+          form.current.reset();
+          toast.success("Message sent succesfully");
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+          toast.error("Some Error Occured");
+        }
+      );
+  };
+
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+
+          <div className="footer-box">
+            <div className="footer-icon">
+              <MdLocationPin />
+            </div>
+            <div className="footer-text">Schwifty Agro Dairy Pvt Lmt <br />
+            T-7, Oberoi Apartment-2, Shyam Nath Marg, Civil Lines 
+            New Delhi, 110054</div>
+          </div>
+
+          <div className="footer-box">
+            <div className="footer-icon">
+              <MdOutlinePhone />
+            </div>
+            <div className="footer-text">+91-8826882623</div>
+          </div>
+
+          <div className="footer-box">
+            <div className="footer-icon">
+              <MdOutlineEmail />
+            </div>
+            <div className="footer-text">info@schwifty.farm</div>
+          </div>
+
+        </div>
+
+        <div className="footer-right">
+          <form ref={form} onSubmit={sendEmail} className="footer-form">
+            <label className="footer-lable">Name</label>
+            <input className="footer-input" type="text" name="user_name" required />
+
+            <label className="footer-lable">Phone Number</label>
+            <input className="footer-input" type="tel" id="phone" name="phone" required />
+
+            <label className="footer-lable">Email</label>
+            <input className="footer-input" type="email" name="user_email" required />
+
+            <label className="footer-lable">Message</label>
+            <textarea className="footer-message" type="text" name="message" required />
+
+            <input className="footer-button" type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Footer;
